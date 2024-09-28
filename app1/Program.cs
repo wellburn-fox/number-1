86c239163c8b034ca51797f44ef61d0c8505085f
wellburn-fox <wellburnfox@gmail.com>
2024-09-28 16:42:44 +0300

    Добавьте файлы проекта.

---
+++ app1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 33;
+            string name = "Довидас";
+            Console.Write($" Ваше имя: { name} вам { age} лет!");
+            
+
+
+                
+
+        }
+    }
+}
